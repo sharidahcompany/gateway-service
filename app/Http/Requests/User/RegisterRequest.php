1ec50ef3468969bf53c491bded806d794156f1e9
ahmedAbdelmoeen <ahmed.abdulmoeen@ceo-app.com>
2026-04-30 17:18:42 +0300

    add regex validation for password in RegisterRequest

--- app/Http/Requests/User/RegisterRequest.php
+++ app/Http/Requests/User/RegisterRequest.php
@@ -45,7 +45,7 @@ class RegisterRequest extends FormRequest
                 'phone:AUTO,INTERNATIONAL', // ستعمل الآن لأن الرقم أصبح يبدأ بـ +
                 'unique:users,phone'
             ],
-            'password'   => ['required', 'string', 'confirmed', 'min:8'],
+            'password'   => ['required', 'string', 'confirmed', 'min:8','regex:/^[A-Za-z0-9]+$/'],
             'status'     => ['sometimes', 'in:active,inactive'],
         ];
     }
